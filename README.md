65d34ae6d03edfabb2d3369d5b481427f36613cb
squirogar <quirogaseb59@gmail.com>
2026-01-05 21:49:11 -0300

    agregados feriados 2026 correcion

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
 # FeriadosChile
+
+*Actualizado el 05-01-2026
 Base de datos y archivo CSV de uso completamente libre de los feriados de Chile 
 a partir del año 2024.
 La iré actualizando a medida que vayan agregando nuevos feriados durante el año. 
 También en el repo está el código con el que se hizo web scraping.
 
 ## Base de datos
-La base de datos fue hecha en mysql.
-Sólo tiene una tabla llamada "feriados".
+La base de datos fue hecha en mysql. Sólo tiene una tabla llamada "feriados".
 
 Los campos son los siguientes:
-- `id` (integer) PRIMARY KEY
-- `fecha` (date) NOT NULL UNIQUE
-- `motivo` (varchar) NOT NULL
-- `tipo` (varchar) NOT NULL
-    - Valores posibles: {`'Religioso'`, `'Civil'`}
+
+- id (integer) PRIMARY KEY
+- fecha (date) NOT NULL UNIQUE
+- motivo (varchar) NOT NULL
+- tipo (varchar) NOT NULL
+	- Valores posibles: {'Religioso', 'Civil'}
+
 
 
 
--- feriados.sql
+++ feriados.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 05-01-2026 a las 21:01:17
+-- Tiempo de generación: 06-01-2026 a las 01:44:41
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -28,71 +28,92 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `feriados` (
-  `id` int(2) DEFAULT NULL,
-  `fecha` varchar(10) DEFAULT NULL,
-  `motivo` varchar(71) DEFAULT NULL,
-  `tipo` varchar(9) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `id` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `motivo` varchar(200) NOT NULL,
+  `tipo` enum('religioso','civil') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `feriados`
 --
 
 INSERT INTO `feriados` (`id`, `fecha`, `motivo`, `tipo`) VALUES
-(1, '01-01-2024', 'Año Nuevo', 'Civil'),
-(2, '29-03-2024', 'Viernes Santo', 'Religioso'),
-(3, '30-03-2024', 'Sábado Santo', 'Religioso'),
-(4, '01-05-2024', 'Día Nacional del Trabajo', 'Civil'),
-(5, '21-05-2024', 'Día de las Glorias Navales', 'Civil'),
-(6, '09-06-2024', 'Elecciones Primarias Alcaldes y Gobernadores', 'Civil'),
-(7, '20-06-2024', 'Día Nacional de los Pueblos Indígenas', 'Civil'),
-(8, '29-06-2024', 'San Pedro y San Pablo', 'Religioso'),
-(9, '16-07-2024', 'Día de la Virgen del Carmen', 'Religioso'),
-(10, '15-08-2024', 'Asunción de la Virgen', 'Religioso'),
-(11, '18-09-2024', 'Independencia Nacional', 'Civil'),
-(12, '19-09-2024', 'Día de las Glorias del Ejército', 'Civil'),
-(13, '20-09-2024', 'Feriado Adicional Fiestas Patrias', 'Civil'),
-(14, '12-10-2024', 'Encuentro de Dos Mundos', 'Civil'),
-(15, '27-10-2024', 'Elecciones Municipales, Consejeros Regionales y Gobernadores Regionales', 'Civil'),
-(16, '31-10-2024', 'Día de las Iglesias Evangélicas y Protestantes', 'Religioso'),
-(17, '01-11-2024', 'Día de Todos los Santos', 'Religioso'),
-(18, '24-11-2024', 'Segunda Vuelta Elección Gobernadores Regionales', 'Civil'),
-(19, '08-12-2024', 'Inmaculada Concepción', 'Religioso'),
-(20, '25-12-2024', 'Navidad', 'Religioso'),
-(21, '01-01-2025', 'Año Nuevo', 'Civil'),
-(22, '18-04-2025', 'Viernes Santo', 'Religioso'),
-(23, '19-04-2025', 'Sábado Santo', 'Religioso'),
-(24, '01-05-2025', 'Día Nacional del Trabajo', 'Civil'),
-(25, '21-05-2025', 'Día de las Glorias Navales', 'Civil'),
-(26, '20-06-2025', 'Día Nacional de los Pueblos Indígenas', 'Civil'),
-(27, '29-06-2025', 'San Pedro y San Pablo', 'Religioso'),
-(28, '16-07-2025', 'Día de la Virgen del Carmen', 'Religioso'),
-(29, '15-08-2025', 'Asunción de la Virgen', 'Religioso'),
-(30, '18-09-2025', 'Independencia Nacional', 'Civil'),
-(31, '19-09-2025', 'Día de las Glorias del Ejército', 'Civil'),
-(32, '12-10-2025', 'Encuentro de Dos Mundos', 'Civil'),
-(33, '31-10-2025', 'Día de las Iglesias Evangélicas y Protestantes', 'Religioso'),
-(34, '01-11-2025', 'Día de Todos los Santos', 'Religioso'),
-(35, '16-11-2025', 'Elecciones Presidenciales y Parlamentarias', 'Civil'),
-(36, '08-12-2025', 'Inmaculada Concepción', 'Religioso'),
-(37, '14-12-2025', 'Elecciones Presidenciales (Segunda Vuelta)', 'Civil'),
-(38, '25-12-2025', 'Navidad', 'Religioso'),
-(39, '01-01-2026', 'Año Nuevo', 'Civil'),
-(40, '03-04-2026', 'Viernes Santo', 'Religioso'),
-(41, '04-04-2026', 'Sábado Santo', 'Religioso'),
-(42, '01-05-2026', 'Día Nacional del Trabajo', 'Civil'),
-(43, '21-05-2026', 'Día de las Glorias Navales', 'Civil'),
-(44, '21-06-2026', 'Día Nacional de los Pueblos Indígenas', 'Civil'),
-(45, '29-06-2026', 'San Pedro y San Pablo', 'Religioso'),
-(46, '16-07-2026', 'Día de la Virgen del Carmen', 'Religioso'),
-(47, '15-08-2026', 'Asunción de la Virgen', 'Religioso'),
-(48, '18-09-2026', 'Independencia Nacional', 'Civil'),
-(49, '19-09-2026', 'Día de las Glorias del Ejército', 'Civil'),
-(50, '12-10-2026', 'Encuentro de Dos Mundos', 'Civil'),
-(51, '31-10-2026', 'Día de las Iglesias Evangélicas y Protestantes', 'Religioso'),
-(52, '01-11-2026', 'Día de Todos los Santos', 'Religioso'),
-(53, '08-12-2026', 'Inmaculada Concepción', 'Religioso'),
-(54, '25-12-2026', 'Navidad', 'Religioso');
+(1, '2024-01-01', 'Año Nuevo', 'civil'),
+(2, '2024-03-29', 'Viernes Santo', 'religioso'),
+(3, '2024-03-30', 'Sábado Santo', 'religioso'),
+(4, '2024-05-01', 'Día Nacional del Trabajo', 'civil'),
+(5, '2024-05-21', 'Día de las Glorias Navales', 'civil'),
+(6, '2024-06-09', 'Elecciones Primarias Alcaldes y Gobernadores', 'civil'),
+(7, '2024-06-20', 'Día Nacional de los Pueblos Indígenas', 'civil'),
+(8, '2024-06-29', 'San Pedro y San Pablo', 'religioso'),
+(9, '2024-07-16', 'Día de la Virgen del Carmen', 'religioso'),
+(10, '2024-08-15', 'Asunción de la Virgen', 'religioso'),
+(11, '2024-09-18', 'Independencia Nacional', 'civil'),
+(12, '2024-09-19', 'Día de las Glorias del Ejército', 'civil'),
+(13, '2024-09-20', 'Feriado Adicional Fiestas Patrias', 'civil'),
+(14, '2024-10-12', 'Encuentro de Dos Mundos', 'civil'),
+(15, '2024-10-27', 'Elecciones Municipales, Consejeros Regionales y Gobernadores Regionales', 'civil'),
+(16, '2024-10-31', 'Día de las Iglesias Evangélicas y Protestantes', 'religioso'),
+(17, '2024-11-01', 'Día de Todos los Santos', 'religioso'),
+(18, '2024-11-24', 'Segunda Vuelta Elección Gobernadores Regionales', 'civil'),
+(19, '2024-12-08', 'Inmaculada Concepción', 'religioso'),
+(20, '2024-12-25', 'Navidad', 'religioso'),
+(21, '2025-01-01', 'Año Nuevo', 'civil'),
+(22, '2025-04-18', 'Viernes Santo', 'religioso'),
+(23, '2025-04-19', 'Sábado Santo', 'religioso'),
+(24, '2025-05-01', 'Día Nacional del Trabajo', 'civil'),
+(25, '2025-05-21', 'Día de las Glorias Navales', 'civil'),
+(26, '2025-06-20', 'Día Nacional de los Pueblos Indígenas', 'civil'),
+(27, '2025-06-29', 'San Pedro y San Pablo', 'religioso'),
+(28, '2025-07-16', 'Día de la Virgen del Carmen', 'religioso'),
+(29, '2025-08-15', 'Asunción de la Virgen', 'religioso'),
+(30, '2025-09-18', 'Independencia Nacional', 'civil'),
+(31, '2025-09-19', 'Día de las Glorias del Ejército', 'civil'),
+(32, '2025-10-12', 'Encuentro de Dos Mundos', 'civil'),
+(33, '2025-10-31', 'Día de las Iglesias Evangélicas y Protestantes', 'religioso'),
+(34, '2025-11-01', 'Día de Todos los Santos', 'religioso'),
+(35, '2025-11-16', 'Elecciones Presidenciales y Parlamentarias', 'civil'),
+(36, '2025-12-08', 'Inmaculada Concepción', 'religioso'),
+(37, '2025-12-14', 'Elecciones Presidenciales (Segunda Vuelta)', 'civil'),
+(38, '2025-12-25', 'Navidad', 'religioso'),
+(39, '2026-01-01', 'Año Nuevo', 'civil'),
+(40, '2026-04-03', 'Viernes Santo', 'religioso'),
+(41, '2026-04-04', 'Sábado Santo', 'religioso'),
+(42, '2026-05-01', 'Día Nacional del Trabajo', 'civil'),
+(43, '2026-05-21', 'Día de las Glorias Navales', 'civil'),
+(44, '2026-06-21', 'Día Nacional de los Pueblos Indígenas', 'civil'),
+(45, '2026-06-29', 'San Pedro y San Pablo', 'religioso'),
+(46, '2026-07-16', 'Día de la Virgen del Carmen', 'religioso'),
+(47, '2026-08-15', 'Asunción de la Virgen', 'religioso'),
+(48, '2026-09-18', 'Independencia Nacional', 'civil'),
+(49, '2026-09-19', 'Día de las Glorias del Ejército', 'civil'),
+(50, '2026-10-12', 'Encuentro de Dos Mundos', 'civil'),
+(51, '2026-10-31', 'Día de las Iglesias Evangélicas y Protestantes', 'religioso'),
+(52, '2026-11-01', 'Día de Todos los Santos', 'religioso'),
+(53, '2026-12-08', 'Inmaculada Concepción', 'religioso'),
+(54, '2026-12-25', 'Navidad', 'religioso');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `feriados`
+--
+ALTER TABLE `feriados`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `fecha` (`fecha`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `feriados`
+--
+ALTER TABLE `feriados`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
